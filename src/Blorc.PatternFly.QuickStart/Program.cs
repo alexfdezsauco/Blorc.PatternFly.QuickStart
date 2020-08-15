841087c04ba842a357cb7f40ad00b9990b4202e4
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-08-15 11:44:22 -0400

    Update to latest stable version of Blazor

--- src/Blorc.PatternFly.QuickStart/Program.cs
+++ src/Blorc.PatternFly.QuickStart/Program.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
-
-namespace Blorc.PatternFly.QuickStart
+﻿namespace Blorc.PatternFly.QuickStart
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using Blorc.PatternFly.Services.Extensions;
+    using Blorc.Services;
+
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBlorcCore();
+            builder.Services.AddBlorcPatternFly();
+
+            builder.Services.AddTransient(
+                sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().MapComponentServices(options => options.MapBlorcPatternFly()).RunAsync();
+        }
     }
 }
